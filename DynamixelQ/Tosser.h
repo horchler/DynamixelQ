97c8f79d468ee6d59a144404cf8f7c1f9baffb59
horchler <adh9@case.edu>
2015-07-13 22:05:20 -0400

    Updated Tosser - new features, cleaner code
    More organized code, added options to control/query board LED. New version option is used by Matlab code for pinging and to decide if a binary file on the computer needs to be uploaded in order to match codes.
    
    Signed-off-by: horchler <adh9@case.edu>

--- DynamixelQ/Tosser.h
+++ DynamixelQ/Tosser.h
@@ -2,13 +2,15 @@
  *	Tosser.h
  *	
  *	Author: Andrew D. Horchler, adh9 @ case.edu
- *	Created: 7-5-15, modified: 7-9-15
+ *	Created: 7-5-15, modified: 7-13-15
  */
 
 #ifndef TOSSER_H_
 #define TOSSER_H_
 
 #include "usb.h"
+
+#define TOSSER_CURRENT_VERSION uint8(0x00)
 
 #define TOSSER_DATA_BUFFER_SIZE uint8(255)
 
@@ -17,21 +19,28 @@
 #define TOSSER_RETURN_PACKET_LENGTH uint8(4)
 
 enum TOSSER_COMMAND{
-	TOSSER_BEGIN  = uint8(0x41), // 65, A
-	TOSSER_PAUSE  = uint8(0x42), // 66, B
-	TOSSER_MICROS = uint8(0x43), // 67, C
-	TOSSER_MILLIS = uint8(0x44), // 68, D
-	TOSSER_STATUS = uint8(0x45), // 69, E
-	TOSSER_BAUD   = uint8(0x46), // 70, E
-	TOSSER_RESET  = uint8(0x47), // 71, G
-	TOSSER_EXIT   = uint8(0x48)  // 72, H
+	TOSSER_BEGIN   = uint8(0x41), // 65, A
+	TOSSER_PAUSE   = uint8(0x42), // 66, B
+	TOSSER_MICROS  = uint8(0x43), // 67, C
+	TOSSER_MILLIS  = uint8(0x44), // 68, D
+	TOSSER_STATUS  = uint8(0x45), // 69, E
+	TOSSER_BAUD    = uint8(0x46), // 70, F
+	TOSSER_VERSION = uint8(0x47), // 71, G
+	TOSSER_GETLED  = uint8(0x48), // 72, H
+	TOSSER_SETLED  = uint8(0x49), // 73, I
+	TOSSER_BLINK   = uint8(0x4A), // 74, J
+	TOSSER_RESET   = uint8(0x4B), // 75, K
+	TOSSER_EXIT    = uint8(0x4C)  // 76, L
 };
 
 typedef enum TOSSER_STATE{
 	TOSSER_WAIT = 0,
 	TOSSER_RUN,
 	TOSSER_STOP
-} TOSSER_STATE; 
+} TOSSER_STATE;
+
+#define TOSSER_BLINK_RUN_DURATION 1e4
+#define TOSSER_BLINK_WAIT_DURATION 1e5
 
 #define TOSSER_DEFAULT_BAUD_RATE_VALUE DXL_DEFAULT_BAUD_RATE_VALUE
 
@@ -85,12 +94,14 @@ private:
 		if (this->bData[0] == TOSSER_PACKET_HEADER && this->bData[1] == TOSSER_PACKET_HEADER && Dxl.isValidChecksum(this->bData, TOSSER_COMMAND_PACKET_LENGTH)) {
 			switch(this->bData[2]) {
 				case TOSSER_BEGIN:
-					if (Dxl.isValidBaudRateValue(this->bData[3]) && this->bData[3] != bTosserBaudRateValue) {
-						this->bTosserBaudRateValue = this->bData[3];
-						Dxl.begin(this->bTosserBaudRateValue);
+					if (Dxl.isValidBaudRateValue(this->bData[3])) {
+						if (this->bData[3] != bTosserBaudRateValue) {
+							this->bTosserBaudRateValue = this->bData[3];
+							Dxl.begin(this->bTosserBaudRateValue);
+						}
 						this->bTosserState = TOSSER_RUN;
 						#ifdef BOARD_H_
-						Board.blink(1e4);
+						Board.blink(TOSSER_BLINK_RUN_DURATION);
 						#endif
 					}
 					break;
@@ -98,61 +109,45 @@ private:
 					if (this->bData[3] == TOSSER_PAUSE) {
 						this->bTosserState = TOSSER_WAIT;
 						#ifdef BOARD_H_
-						Board.blink(1e5);
+						Board.blink(TOSSER_BLINK_WAIT_DURATION);
 						#endif
 					}
 					break;
 				case TOSSER_MICROS:
-					if (this->bData[3] == TOSSER_MICROS) {
-						this->uTosserTime = microsec();
-						this->bData[0] = TOSSER_PACKET_HEADER;
-						this->bData[1] = TOSSER_PACKET_HEADER;
-						this->bData[2] = TOSSER_MICROS;
-						this->bData[3] = (this->uTosserTime & 0xFF);
-						this->bData[4] = ((this->uTosserTime >> 8) & 0xFF);
-						this->bData[5] = ((this->uTosserTime >> 16) & 0xFF);
-						this->bData[6] = ((this->uTosserTime >> 24) & 0xFF);
-						this->bData[7] = ~(this->bData[2]+this->bData[3]+this->bData[4]+this->bData[5]+this->bData[6]);
-						this->bNum = TOSSER_RETURN_PACKET_LENGTH+4;
-						this->USBprintRaw(this->bData, this->bNum);
-					}
+					txPacket(TOSSER_MICROS, (uint32)microsec());
 					break;
 				case TOSSER_MILLIS:
-					if (this->bData[3] == TOSSER_MILLIS) {
-						this->uTosserTime = millisec();
-						this->bData[0] = TOSSER_PACKET_HEADER;
-						this->bData[1] = TOSSER_PACKET_HEADER;
-						this->bData[2] = TOSSER_MILLIS;
-						this->bData[3] = (this->uTosserTime & 0xFF);
-						this->bData[4] = ((this->uTosserTime >> 8) & 0xFF);
-						this->bData[5] = ((this->uTosserTime >> 16) & 0xFF);
-						this->bData[6] = ((this->uTosserTime >> 24) & 0xFF);
-						this->bData[7] = ~(this->bData[2]+this->bData[3]+this->bData[4]+this->bData[5]+this->bData[6]);
-						this->bNum = TOSSER_RETURN_PACKET_LENGTH+4;
-						this->USBprintRaw(this->bData, this->bNum);
-					}
+					txPacket(TOSSER_MILLIS, (uint32)millisec());
 					break;
 				case TOSSER_STATUS:
-					if (this->bData[3] == TOSSER_STATUS) {
-						this->bData[0] = TOSSER_PACKET_HEADER;
-						this->bData[1] = TOSSER_PACKET_HEADER;
-						this->bData[2] = TOSSER_STATUS;
-						this->bData[3] = this->bTosserState;
-						this->bData[4] = ~(this->bData[2]+this->bData[3]);
-						this->bNum = TOSSER_RETURN_PACKET_LENGTH+1;
-						this->USBprintRaw(this->bData, this->bNum);
-					}
+					txPacket(TOSSER_STATUS, (uint8)this->bTosserState);
 					break;
 				case TOSSER_BAUD:
-					if (this->bData[3] == TOSSER_BAUD) {
-						this->bData[0] = TOSSER_PACKET_HEADER;
-						this->bData[1] = TOSSER_PACKET_HEADER;
-						this->bData[2] = TOSSER_BAUD;
-						this->bData[3] = this->bTosserBaudRateValue;
-						this->bData[4] = ~(this->bData[2]+this->bData[3]);
-						this->bNum = TOSSER_RETURN_PACKET_LENGTH+1;
-						this->USBprintRaw(this->bData, this->bNum);
+					txPacket(TOSSER_BAUD, this->bTosserBaudRateValue);
+					break;
+				case TOSSER_VERSION:
+					txPacket(TOSSER_VERSION, TOSSER_CURRENT_VERSION);
+					break;
+				case TOSSER_GETLED:
+					#ifdef BOARD_H_
+					txPacket(TOSSER_GETLED, Board.getLED());
+					#else
+					txPacket(TOSSER_GETLED, (uint8)255);
+					#endif
+					break;
+				case TOSSER_SETLED:
+					#ifdef BOARD_H_
+					if (this->bData[3] == 0 || this->bData[3] == 1) {
+						Board.setLED(this->bData[3]);
 					}
+					#endif
+					break;
+				case TOSSER_BLINK:
+					#ifdef BOARD_H_
+					if (this->bData[3] == TOSSER_BLINK) {
+						Board.blink((this->bTosserState == TOSSER_RUN) ? TOSSER_BLINK_RUN_DURATION : TOSSER_BLINK_WAIT_DURATION);
+					}
+					#endif
 					break;
 				case TOSSER_RESET:
 					if (this->bData[3] == TOSSER_RESET) {
@@ -160,7 +155,7 @@ private:
 						Dxl.begin(this->bTosserBaudRateValue);
 						this->bTosserState = TOSSER_RUN;
 						#ifdef BOARD_H_
-						Board.blink(1e4);
+						Board.blink(TOSSER_BLINK_RUN_DURATION);
 						#endif
 					}
 					break;
@@ -175,6 +170,35 @@ private:
 				default:
 					break;
 			}
+		}
+	}
+	
+	inline void txPacket(const uint8 CommandName, const uint8 Data)
+	{
+		if (this->bData[3] == CommandName) {
+			this->bData[0] = TOSSER_PACKET_HEADER;
+			this->bData[1] = TOSSER_PACKET_HEADER;
+			this->bData[2] = CommandName;
+			this->bData[3] = Data;
+			this->bData[4] = ~(this->bData[2]+this->bData[3]);
+			this->bNum = TOSSER_RETURN_PACKET_LENGTH+1;
+			this->USBprintRaw(this->bData, this->bNum);
+		}
+	}
+	
+	inline void txPacket(const uint8 CommandName, const uint32 Data)
+	{
+		if (this->bData[3] == CommandName) {
+			this->bData[0] = TOSSER_PACKET_HEADER;
+			this->bData[1] = TOSSER_PACKET_HEADER;
+			this->bData[2] = CommandName;
+			this->bData[3] = (Data & 0xFF);
+			this->bData[4] = ((Data >> 8) & 0xFF);
+			this->bData[5] = ((Data >> 16) & 0xFF);
+			this->bData[6] = ((Data >> 24) & 0xFF);
+			this->bData[7] = ~(this->bData[2]+this->bData[3]+this->bData[4]+this->bData[5]+this->bData[6]);
+			this->bNum = TOSSER_RETURN_PACKET_LENGTH+4;
+			this->USBprintRaw(this->bData, this->bNum);
 		}
 	}
 	
@@ -198,7 +222,6 @@ private:
 	TOSSER_STATE bTosserState;
 	uint8 bTosserBaudRateValue, bResetBaudRateValue;
 	uint8 bNum, bData[TOSSER_DATA_BUFFER_SIZE];
-	uint32 uTosserTime;
 };
 
 // Instantiate Tosser object
